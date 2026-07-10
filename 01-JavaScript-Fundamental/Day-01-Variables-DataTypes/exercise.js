e119a27a50eb7118764aaf2f77df3886c4c21e86
sayyeone <iamadistyfa002@gmail.com>
2026-07-10 20:56:50 +0700

    feat: add result exc lv 1

---
+++ 01-JavaScript-Fundamental/Day-01-Variables-DataTypes/exercise.js
@@ -0,0 +1,38 @@
+// LV1
+
+// 1
+let firstName = "Adisty";
+let lastName = "Ardani";
+let age = 20;
+let isStudent = true;
+
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+console.log(isStudent);
+
+// 2
+console.log("tipe data nama:", typeof(firstName));
+console.log("tipe data nama:", typeof(lastName));
+console.log("tipe data umur:", typeof(age));
+console.log("tipe data status:", typeof(isStudent));
+
+// 3
+let a = 10;
+let b = 12;
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
+
+// 4
+let rad = 7;
+const pi = 3.14;
+console.log(pi * rad ** 2);
+
+// 5
+let celcius = 30;
+console.log((celcius * 9/5) + 32);
+
+
